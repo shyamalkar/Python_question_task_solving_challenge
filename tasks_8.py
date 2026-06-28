7332eb1fcbb956a2f8671d74597b0384eeecfccc
Shyamal kar <190195736+shyamalkar@users.noreply.github.com>
2026-06-28 09:33:54 +0530

    Update tasks_8.py

--- tasks_8.py
+++ tasks_8.py
@@ -7,6 +7,9 @@ seconds =  int(input("Ënter a total second:"))
 minutes = seconds // 60
 remaining_seconds = seconds % 60 
 
+
+
+
 print("Minutes:", minutes)
 print("Remaining seconds:", remaining_seconds)
 
