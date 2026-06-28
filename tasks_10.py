65e58bb91cb811a0aa0b3f1e31e56b34ba940b84
Shyamal kar <190195736+shyamalkar@users.noreply.github.com>
2026-06-28 09:33:44 +0530

    Update tasks_10.py

--- tasks_10.py
+++ tasks_10.py
@@ -3,4 +3,8 @@ number = input("Enter a number :")
 print("original types", type(number))
 number = float(number)
 print("New type:", type(number))
-print("Number raised to the power of 3:", number ** 3 )
+print("Number raised to the power of 3:", number ** 3 )   
+
+
+
+
