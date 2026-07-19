ae8f11669fd47573aa353a6a96c96b5622255bde
Shyamal kar <190195736+shyamalkar@users.noreply.github.com>
2026-07-19 22:26:51 +0530

    Update Taks_1.py

--- challenge_1_folder/Taks_1.py
+++ challenge_1_folder/Taks_1.py
@@ -10,7 +10,7 @@ radius = float(input("Enter the radius of the circle:"))
 
 area = pi * radius ** 2
 print("The area of the circle is:", area)
-
+ 
 # Explanation 
 """
 inputs() gets the radius as a string. 
